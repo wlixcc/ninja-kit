ad600a9af43bad869269010ace8231fc7ec5365e
william <>
2020-02-24 16:02:59 +0800

    1. test action

---
+++ Example/Pods/Target Support Files/Pods-ninja-kit_Tests/Pods-ninja-kit_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ninja_kit_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_ninja_kit_Tests
+@end
---
+++ Example/Pods/Target Support Files/ninja-kit/ninja-kit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ninja_kit : NSObject
+@end
+@implementation PodsDummy_ninja_kit
+@end
