cb608298d7f318e4c20f11c769580977702460f3
william <>
2020-02-24 17:17:09 +0800

    1. update

---
+++ ninja-kit/Classes/UIView+NJ.h
@@ -0,0 +1,20 @@
+//
+//  UIView+NJ.h
+//  ninja-kit_Example
+//
+//  Created by wl on 2020/2/22.
+//  Copyright © 2020 wl. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIView (NJ)
+
+- (void)nj_ChangeBackgroundColor2Red;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ninja-kit/Classes/UIView+NJ.m
@@ -0,0 +1,18 @@
+//
+//  UIView+NJ.m
+//  ninja-kit_Example
+//
+//  Created by wl on 2020/2/22.
+//  Copyright © 2020 wl. All rights reserved.
+//
+
+#import "UIView+NJ.h"
+
+
+@implementation UIView (NJ)
+
+- (void)nj_ChangeBackgroundColor2Red {
+    self.backgroundColor = [UIColor redColor];
+}
+
+@end
